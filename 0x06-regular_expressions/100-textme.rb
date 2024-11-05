d4fed909a6cf7cb253e46dd402de4f158c979702
veronica1013 <veronica.isiaho@gmail.com>
2024-11-05 14:55:46 -0800

    Adding a file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([^\[\]]+)\].\[to:([^\[\]]+)\].\[flags:([^\[\]]+)\]/).join(',')
+
+log_entry = ARGV[0]
+
+# Regular expression to capture sender, receiver, and flags
+match_data = log_entry.match(/\[from:([^]]+)\] \[to:([^]]+)\] \[flags:([^]]+)\]/)
+
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+  
+  # Output the required format: [SENDER],[RECEIVER],[FLAGS]
+  puts "#{sender},#{receiver},#{flags}"
+else
+  puts "No match found"
+end
