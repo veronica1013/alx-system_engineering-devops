8306b99902a8ddf3a81e9038193f94d0e2f36a51
veronica1013 <veronica.isiaho@gmail.com>
2024-11-05 14:46:30 -0800

    Adding a file

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^hbt{0,4}n$/).join
+
+# Check if there is an argument provided
+if ARGV[0]
+  # Scan the argument with the regex and join results if matched
+  puts ARGV[0].scan(/^hbt{0,4}n$/).join
+else
+  puts "Please provide an argument."
+end
