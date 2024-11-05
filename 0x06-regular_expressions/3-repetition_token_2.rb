d7296b5aee744d89c33d68c2b9ffda603a1c7390
veronica1013 <veronica.isiaho@gmail.com>
2024-11-05 14:39:00 -0800

    Adding a file

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-
-if ARGV[0]
-  match = ARGV[0].match(/^hb{1}t{1,4}n$/)
-  if match
-    puts match[0]
-  else
-    puts "No match"
-  end
-else
-  puts "Please provide an argument."
-end
+puts ARGV[0].scan(/hbt+n/).join
